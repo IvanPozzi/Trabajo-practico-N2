540cea173fbe33725cbee4fd92a79a745f702616
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2024-09-06 00:44:37 -0300

    Se craron las clases Marca y Categoria

---
+++ Trabajo Practico N2/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Practico_N2
+{
+    internal class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
