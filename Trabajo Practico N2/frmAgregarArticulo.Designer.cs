803db6fe857f66d5e8ff12164dc3d7670bfac255
IvanPozzi <c.ivanpozzi@gmail.com>
2024-09-11 01:41:13 -0300

    agregue el campo para agregar la url de la imagen en agregar articulo y modifique la estetica de la ventana

--- Trabajo Practico N2/frmAgregarArticulo.Designer.cs
+++ Trabajo Practico N2/frmAgregarArticulo.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgregarArticulo));
             this.lvlCodArticulo = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
             this.lblDescripcion = new System.Windows.Forms.Label();
@@ -43,72 +44,86 @@
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.lblAgregarArticuloTitulo = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lvlCodArticulo
             // 
             this.lvlCodArticulo.AutoSize = true;
-            this.lvlCodArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvlCodArticulo.Location = new System.Drawing.Point(152, 118);
+            this.lvlCodArticulo.BackColor = System.Drawing.Color.Transparent;
+            this.lvlCodArticulo.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlCodArticulo.ForeColor = System.Drawing.Color.Orange;
+            this.lvlCodArticulo.Location = new System.Drawing.Point(47, 77);
             this.lvlCodArticulo.Name = "lvlCodArticulo";
-            this.lvlCodArticulo.Size = new System.Drawing.Size(212, 29);
+            this.lvlCodArticulo.Size = new System.Drawing.Size(207, 29);
             this.lvlCodArticulo.TabIndex = 0;
             this.lvlCodArticulo.Text = "Código de Artículo";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(152, 168);
+            this.lblNombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblNombre.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.Color.Orange;
+            this.lblNombre.Location = new System.Drawing.Point(47, 140);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(107, 29);
+            this.lblNombre.Size = new System.Drawing.Size(102, 29);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre:";
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescripcion.Location = new System.Drawing.Point(152, 224);
+            this.lblDescripcion.BackColor = System.Drawing.Color.Transparent;
+            this.lblDescripcion.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.ForeColor = System.Drawing.Color.Orange;
+            this.lblDescripcion.Location = new System.Drawing.Point(47, 199);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(147, 29);
+            this.lblDescripcion.Size = new System.Drawing.Size(148, 29);
             this.lblDescripcion.TabIndex = 2;
             this.lblDescripcion.Text = "Descripcion:";
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMarca.Location = new System.Drawing.Point(152, 384);
+            this.lblMarca.BackColor = System.Drawing.Color.Transparent;
+            this.lblMarca.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.ForeColor = System.Drawing.Color.Orange;
+            this.lblMarca.Location = new System.Drawing.Point(47, 313);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(79, 29);
+            this.lblMarca.Size = new System.Drawing.Size(80, 29);
             this.lblMarca.TabIndex = 3;
             this.lblMarca.Text = "Marca";
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(152, 439);
+            this.lblCategoria.BackColor = System.Drawing.Color.Transparent;
+            this.lblCategoria.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.ForeColor = System.Drawing.Color.Orange;
+            this.lblCategoria.Location = new System.Drawing.Point(47, 397);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(118, 29);
+            this.lblCategoria.Size = new System.Drawing.Size(117, 29);
             this.lblCategoria.TabIndex = 4;
             this.lblCategoria.Text = "Categoria";
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrecio.Location = new System.Drawing.Point(152, 508);
+            this.lblPrecio.BackColor = System.Drawing.Color.Transparent;
+            this.lblPrecio.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.ForeColor = System.Drawing.Color.Orange;
+            this.lblPrecio.Location = new System.Drawing.Point(47, 468);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(89, 29);
+            this.lblPrecio.Size = new System.Drawing.Size(88, 29);
             this.lblPrecio.TabIndex = 5;
             this.lblPrecio.Text = "Precio:";
             // 
             // txtcadigo
             // 
             this.txtcadigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtcadigo.Location = new System.Drawing.Point(384, 118);
+            this.txtcadigo.Location = new System.Drawing.Point(287, 77);
             this.txtcadigo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtcadigo.Name = "txtcadigo";
             this.txtcadigo.Size = new System.Drawing.Size(198, 35);
@@ -117,17 +132,17 @@
             // txtdescripcion
             // 
             this.txtdescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtdescripcion.Location = new System.Drawing.Point(384, 224);
+            this.txtdescripcion.Location = new System.Drawing.Point(287, 199);
             this.txtdescripcion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtdescripcion.Multiline = true;
             this.txtdescripcion.Name = "txtdescripcion";
-            this.txtdescripcion.Size = new System.Drawing.Size(499, 119);
+            this.txtdescripcion.Size = new System.Drawing.Size(499, 92);
             this.txtdescripcion.TabIndex = 7;
             // 
             // txtnombre
             // 
             this.txtnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtnombre.Location = new System.Drawing.Point(384, 168);
+            this.txtnombre.Location = new System.Drawing.Point(287, 140);
             this.txtnombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtnombre.Name = "txtnombre";
             this.txtnombre.Size = new System.Drawing.Size(298, 35);
@@ -137,7 +152,7 @@
             // 
             this.cmbmarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbmarca.FormattingEnabled = true;
-            this.cmbmarca.Location = new System.Drawing.Point(384, 372);
+            this.cmbmarca.Location = new System.Drawing.Point(287, 313);
             this.cmbmarca.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbmarca.Name = "cmbmarca";
             this.cmbmarca.Size = new System.Drawing.Size(298, 37);
@@ -147,7 +162,7 @@
             // 
             this.cmbcategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbcategoria.FormattingEnabled = true;
-            this.cmbcategoria.Location = new System.Drawing.Point(384, 435);
+            this.cmbcategoria.Location = new System.Drawing.Point(287, 394);
             this.cmbcategoria.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbcategoria.Name = "cmbcategoria";
             this.cmbcategoria.Size = new System.Drawing.Size(298, 37);
@@ -156,7 +171,7 @@
             // txtprecio
             // 
             this.txtprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtprecio.Location = new System.Drawing.Point(384, 500);
+            this.txtprecio.Location = new System.Drawing.Point(287, 465);
             this.txtprecio.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtprecio.Name = "txtprecio";
             this.txtprecio.Size = new System.Drawing.Size(298, 35);
@@ -164,43 +179,77 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.Location = new System.Drawing.Point(304, 577);
+            this.btnAgregar.BackColor = System.Drawing.Color.Transparent;
+            this.btnAgregar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAgregar.BackgroundImage")));
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.Color.Orange;
+            this.btnAgregar.Location = new System.Drawing.Point(638, 330);
             this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(148, 82);
             this.btnAgregar.TabIndex = 12;
             this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(617, 577);
+            this.btnCancelar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.Orange;
+            this.btnCancelar.Location = new System.Drawing.Point(638, 475);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(148, 82);
             this.btnCancelar.TabIndex = 13;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // lblAgregarArticuloTitulo
             // 
             this.lblAgregarArticuloTitulo.AutoSize = true;
-            this.lblAgregarArticuloTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAgregarArticuloTitulo.Location = new System.Drawing.Point(404, 34);
+            this.lblAgregarArticuloTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblAgregarArticuloTitulo.Font = new System.Drawing.Font("Impact", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgregarArticuloTitulo.ForeColor = System.Drawing.Color.Orange;
+            this.lblAgregarArticuloTitulo.Location = new System.Drawing.Point(321, 9);
             this.lblAgregarArticuloTitulo.Name = "lblAgregarArticuloTitulo";
-            this.lblAgregarArticuloTitulo.Size = new System.Drawing.Size(260, 38);
+            this.lblAgregarArticuloTitulo.Size = new System.Drawing.Size(253, 39);
             this.lblAgregarArticuloTitulo.TabIndex = 14;
             this.lblAgregarArticuloTitulo.Text = "Agregar Artículo.";
+            // 
+            // url
+            // 
+            this.url.AutoSize = true;
+            this.url.BackColor = System.Drawing.Color.Transparent;
+            this.url.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.url.ForeColor = System.Drawing.Color.Orange;
+            this.url.Location = new System.Drawing.Point(47, 528);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(136, 29);
+            this.url.TabIndex = 15;
+            this.url.Text = "URL imagen";
+            // 
+            // txturl
+            // 
+            this.txturl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txturl.Location = new System.Drawing.Point(287, 522);
+            this.txturl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(298, 35);
+            this.txturl.TabIndex = 16;
             // 
             // frmAgregarArticulo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 676);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(859, 611);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.url);
             this.Controls.Add(this.lblAgregarArticuloTitulo);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAgregar);
@@ -242,5 +291,7 @@
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Label lblAgregarArticuloTitulo;
+        private System.Windows.Forms.Label url;
+        private System.Windows.Forms.TextBox txturl;
     }
 }
